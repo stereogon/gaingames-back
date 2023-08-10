4da7dfee2471ba0b43da41ec24738b09a4dd6f32
Chandan Gupta <me.chandangupta3110@gmail.com>
2023-08-10 10:46:14 +0530

    Update SpringFitnessAppApplication.java

--- src/main/java/com/fitnessapp/SpringFitnessAppApplication.java
+++ src/main/java/com/fitnessapp/SpringFitnessAppApplication.java
@@ -3,7 +3,7 @@ package com.fitnessapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class SpringFitnessAppApplication {
 
 	public static void main(String[] args) {
